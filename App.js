dace48bf56f3cf1516275742cd4d57df14ad6e8c
Ayman Syam <aymansyam97@gmail.com>
2022-12-01 00:18:24 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import quran from './assets/quran.json';
+
 
 export default function App() {
+
+  getRandomAyah = () => {
+    const surah = quran[Math.floor(Math.random() * quran.length)];
+    const ayah = surah.ayahs[Math.floor(Math.random() * surah.ayahs.length)];
+    console.log(ayah['text']);
+    return ayah['text'];
+
+  }
+
   return (
+    // center view on top of screen
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{getRandomAyah()}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +28,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'top',
+    marginTop: 100,
+    
   },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold', 
+  }
 });
+
+// q: set remote url git
+// a: git remote add origin
