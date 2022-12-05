a5649b0081ad2535beb4aaa9e29d0075aa1155d4
Ayman Syam <aymansyam97@gmail.com>
2022-12-05 11:54:04 +0200

    updated docs

--- components/Rules.js
+++ components/Rules.js
@@ -12,7 +12,20 @@ function Rules() {
       </Text>
       <Text>2. You have 10 rounds to earn as many points as possible.</Text>
       <Text>
-        3. The difficulty level determines the multiplier of points you earn for
+        3. The difficulty level determines from which Surahs to pull the Ayahs,
+        and the amount of time you have per guess. The tier is as following:
+      </Text>
+      <Text></Text>
+      <Text>
+        Easy: 1/3 score multiplier, 75 seconds per guess, Surahs 100-114
+      </Text>
+      <Text>
+        Medium: 1/2 score multiplier, 50 seconds per guess, Surahs 50-114
+      </Text>
+      <Text>Hard: normal score, 25 seconds per guess, all Surahs</Text>
+      <Text></Text>
+      <Text>
+        The difficulty level determines the multiplier of points you earn for
         each correct guess.
       </Text>
       <Text>
